c403aa44acc501bafab3460e395e49f0fa2d7083
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-23 23:32:51 -0400

    change ActiveRecord version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
-  
+
+  get "/login" do
+    "login form"
+  end
+
+  get "/signup" do
+    "signup form"
+  end
+
+  get "/logout" do
+    "logout form"
+  end
 end
