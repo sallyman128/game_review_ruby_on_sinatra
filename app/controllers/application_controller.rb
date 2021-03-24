3efcdadc9da797f661459b949259d4b694ba0625
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-23 23:18:28 -0400

    enable sessions

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "mod2_final_project"
+  end
+
+
+
+  get "/" do
+    erb :welcome
+  end
+
+end
