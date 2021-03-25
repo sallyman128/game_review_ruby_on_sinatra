f12deb2e1a45dfb4cb59c8b60d9051023703505a
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-24 22:11:46 -0400

    create game, user, and usergame models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :user
+  has_many :user_games
+  has_many :users, through: :user_games
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :games
+  has_many :user_games
+  has_many :games, through: :user_games
 end
