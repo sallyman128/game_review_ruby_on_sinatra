8571c74106a9d9ffbfd2c4fa38167f1deffa84fc
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-26 22:57:02 -0400

    add new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,30 @@
 class GamesController < ApplicationController
   get "/games" do
     @games = Game.all
-    erb :"./games/index"
+    erb :"/games/index"
   end
 
   get "/games/new" do
-    erb :"./games/new"
+    erb :"/games/new"
   end
 
   get "/games/:id" do
     @game = Game.find(params[:id])
-    erb :"./games/show"
+    erb :"/games/show"
+  end
+
+  post "/games" do
+    game = Game.new()
+    game.name = params[:name]
+    game.max_player = params[:max_player]
+    game.description = params[:description]
+    game.save
+    redirect to :"/games/#{game.id}"
+  end
+
+  delete "/games/:id" do
+    game = Game.find(params[:id])
+    game.destroy
+    redirect to :"/games"
   end
 end
