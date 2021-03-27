2dd74897a902fc48c1c49f5805bbf19b93b944d9
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-27 18:38:01 -0400

    allow user to delete their own post

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,12 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     erb :"/reviews/edit"
   end
+
+  get "/reviews/:id/delete" do
+    @review = Review.find(params[:id])
+    erb :"/reviews/delete"
+  end
+
+  delete "/reviews/:id" do
+  end
 end
