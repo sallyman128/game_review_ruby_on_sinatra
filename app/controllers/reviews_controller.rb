2e4fb50e0200b45624d4384eae0f13e7e0a363a9
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-27 22:48:58 -0400

    final cleanups

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,30 @@
 class ReviewsController < ApplicationController
 
   get "/games/:game_id/reviews/:review_id/edit" do
-    @review = Review.find(params[:id])
-    erb :"/reviews/edit"
+    @review = Review.find(params[:review_id])
+    if @review.user.email == session[:email]
+      erb :"/reviews/edit"
+    else
+      redirect to "/login"
+    end
   end
 
   get "/games/:game_id/reviews/:review_id/delete" do
-    @review = Review.find(params[:id])
-    erb :"/reviews/delete"
+    @review = Review.find(params[:review_id])
+    if @review.user.email == session[:email]
+      erb :"/reviews/delete"
+    else
+      redirect to "/login"
+    end
   end
 
   get "/games/:game_id/reviews/new" do
-    @game = Game.find(params[:game_id])
-    erb :"/reviews/new"
+    if session[:email]
+      @game = Game.find(params[:game_id])
+      erb :"/reviews/new"
+    else
+      redirect to "/login"
+    end
   end
 
   post "/:game_id/reviews" do
